41d07ae2862a493d720fc04dfdaf55f18d23095e
OCDkirby <mangacreeperyt@gmail.com>
2022-02-11 12:14:50 -0600

    Complete DFA portion

--- src/haskell/lexical.hs
+++ src/haskell/lexical.hs
@@ -6,50 +6,74 @@
 {-# LANGUAGE CApiFFI #-} -- Allows better imports with capi as opposed to ccall
 
 module Lexical where
-  
+
 import Foreign.C.Types
 import Foreign.C.String
 import Data.List
 import Data.Maybe
 import Data.Char (ord)
+import Data.Array
 
+-- Make certain C functions available to the linker
 foreign import capi "lexicalli/interface.h get_char" get_char :: IO CChar
 foreign import capi "lexicalli/interface.h put_token" put_token :: CString -> CInt -> CString -> CInt -> CInt 
 
+-- Helper IO
+-- Wrapper for getting the character via C
 get_char_cast :: IO Char
 get_char_cast = do
   c <- get_char
   return (castCCharToChar c)
+-- Easy debug printing for returns from the main dfa function
+print_str_int_char :: (String, Int, Char) -> IO ()
+print_str_int_char (s, i, c) = do
+  putStrLn s
+  print i
+  print c
 
+-- Main DFA structure  
+-- Generic type holding Sigma, delta, q_0, and F for a DFSA, needs types for its nonterminals and terminals
+-- Called T, M, S, Z in the notes, I used the mathematical terms because I built this
+--  before we covered it in class
+-- Deviations from mathematics: Doesn't require a state alphabet as 
+--  the state type is a generic parameter, so Q/K is removed from the tuple
+type D_F_Automaton state term = ([term], (state->term->state), state, (state->Bool))
+
+-- Alphabet building, currently some parts are unused
 symbols = "+-*/{}()=!<>,"
 digits = "1234567890"
 letters = "zxcvbnmasdfghjklqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP"
 whitespace = "\n\t "
-alphabet = symbols ++ digits ++ letters ++ whitespace
+end_file = '\255'
+alphabet = symbols ++ digits ++ letters ++ whitespace ++ [end_file]
 
-state_matrix = [[ 5, 3, 1, 7, 11, 14, 0, 1 ],
-                [ 1, 1, 1, 1, 1, 1, 1, 1 ], -- Error state
-                [ 2, 2, 2, 2, 2, 2, 2, 1 ], -- Multiplication operator state
-                [ 4, 3, 4, 4, 4, 4, 4, 1 ],
-                [ 4, 4, 4, 4, 4, 4, 4, 1 ], -- Integer state
-                [ 5, 5, 6, 6, 6, 6, 6, 1 ],
-                [ 6, 6, 6, 6, 6, 6, 6, 1 ], -- Var state
-                [ 10, 10, 8, 10, 10, 10, 10, 1 ],
-                [ 8, 8, 9, 8, 8, 8, 8, 8 ], -- Comment state
-                [ 8, 8, 8, 0, 8, 8, 8, 1 ],
-                [ 10, 10, 10, 10, 10, 10, 10, 1 ], -- Division operator state
-                [ 12, 12, 12, 12, 13, 12, 12, 1 ], 
-                [ 12, 12, 12, 12, 12, 12, 12, 1 ], -- Assignment operator
-                [ 13, 13, 13, 13, 13, 13, 13, 1 ], -- Comparison operator
-                [ 15, 15, 15, 15, 16, 15, 15, 1 ],
-                [ 15, 15, 15, 15, 15, 15, 15, 1 ], -- < Operator 
-                [ 16, 16, 16, 16, 16, 16, 16, 1 ]] -- <= Operator
-                
-                
-end_states = [ 1, 2, 4, 6, 10, 12, 13, 15, 16 ]
-is_end :: Int -> Bool
-is_end x = elem x end_states
 
+-- Structures and subroutines requiring modification later as I add more to the language
+reserved_words = ["CLASS", "VAR", "CONST"]
+-- Note: the end states are just rows of their number because the value doesn't matter
+--                L, D, *, /, =, <, ws, e, semi
+state_matrix = [[ 5, 3, 2, 7, 11, 14, 0, 1, 17 ],
+                [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], -- Error state
+                [ 18, 18, 18, 18, 18, 18, 18, 1, 1],
+                [ 1, 3, 4, 4, 4, 4, 4, 1, 4 ],
+                [ 4, 4, 4, 4, 4, 4, 4, 4, 4 ], -- Integer state
+                [ 5, 5, 6, 6, 6, 6, 6, 1, 6 ],
+                [ 6, 6, 6, 6, 6, 6, 6, 6, 6 ], -- Var state
+                [ 10, 10, 8, 10, 10, 10, 10, 1, 1 ],
+                [ 8, 8, 9, 8, 8, 8, 8, 8, 8 ], -- /*
+                [ 8, 8, 8, 0, 8, 8, 8, 8, 8 ], -- */
+                [ 10, 10, 10, 10, 10, 10, 10, 10, 10 ], -- Division operator state
+                [ 12, 12, 1, 1, 13, 1, 12, 1, 1 ], 
+                [ 12, 12, 12, 12, 12, 12, 12, 12, 12 ], -- Assignment operator
+                [ 13, 13, 13, 13, 13, 13, 13, 13, 13 ], -- Comparison operator
+                [ 15, 15, 1, 1, 16, 15, 15, 1, 1 ],
+                [ 15, 15, 15, 15, 15, 15, 15, 15, 15 ], -- < Operator 
+                [ 19, 19, 1, 1, 1, 1, 19, 1, 1 ],
+                [ 17, 17, 17, 17, 17, 17, 17, 17, 17 ], -- semicolon
+                [ 18, 18, 18, 18, 18, 18, 18, 18, 18], -- Multiplication operator
+                [ 19, 19, 19, 19, 19, 19, 19, 19, 19]] -- <= Operator                            
+end_states = [ 1, 4, 6, 10, 12, 13, 15, 17, 18, 19]
+skip_states = [0, 8, 9]
 state_transition :: Int -> Char -> Int
 state_transition s c | (elem c letters)    = state_matrix!!s!!0
                      | (elem c digits)     = state_matrix!!s!!1
@@ -58,43 +82,73 @@ state_transition s c | (elem c letters)    = state_matrix!!s!!0
                      | (c == '=')          = state_matrix!!s!!4
                      | (c == '<')          = state_matrix!!s!!5
                      | (elem c whitespace) = state_matrix!!s!!6
+                     | (c == ';')          = state_matrix!!s!!8 
                      | otherwise           = state_matrix!!s!!7
 
 
+-- Unchanging helper subroutines
+is_end :: Int -> Bool
+is_end x = elem x end_states
+skip_state :: Int -> Bool
+skip_state s = elem s skip_states
 
--- Generic type holding Q, Sigma, delta, q_0, and F for a DFSA, needs types for its nonterminals and terminals
--- Called K, T, M, S, Z in the notes, I used the mathematical terms because I built this
---  before we covered it in class
-type D_F_Automaton state term = ([state], [term], (state->term->state), state, (state->Bool))
--- Function to check for valid grammar from an input automaton, a partial token, and a terminal callback
+-- Main DFA scanner, scans character by character and returns the newest token
+-- Function to check for valid grammar from an input automaton, a partial token, and a state skip callback
 -- Returns the next list of terminals denoting a token, its class, and the start token to resume parsing on
-scan :: D_F_Automaton state term -> [term] -> (IO term) -> IO ([term], state, term)
-scan (q, sigma, delta, s, f) token gt = do
-  -- Get the terminal at the head of the input stream, character by character
+scan :: D_F_Automaton state term -> [term] -> (IO term) -> (state -> Bool) -> IO ([term], state, term)
+scan (sigma, delta, s, f) token gt sk = do
   t <- gt
   
-  -- Get a new state based on current state and terminal
-  let st = delta s t 
-  
+  --  Get the new state based on leftover token from last parse
+  let st = delta s t
   -- Check for valid end-of-token, otherwise recurse with new state and slightly longer partial token
-  if f st then return (token, s, t)
+  if f st then return (token, st, t)
   else do
-    ret <- (scan (q, sigma, delta, st, f) (token ++ [t]) gt)
-    return ret
+    if sk st then do -- Don't include this terminal in the token, leading whitespace etc
+      ret <- (scan (sigma, delta, st, f) (token) gt sk) 
+      return ret
+    else do
+      ret <- (scan (sigma, delta, st, f) (token ++ [t]) gt sk) 
+      return ret
 
+-- Loop calling DFA scanner and writing tokens to the intermediate file
+-- While running this function my computer completely filled with bees.
+-- I think they must have a hive inside the log, because it wouldn't
+--  make sense for them to have a hive inside the recursion.
+-- (they couldn't store any honey in the comb beyond depth 1)
+log_and_recurse :: (String, Int, Char) -> IO () 
+log_and_recurse (token, cls, ch) = do
+  print_str_int_char (token, cls, ch) -- Debug
+  putStrLn ""
+  
+  -- TODO Error checking based on current token and character
+  if cls == 1 then putStrLn "Lexicalli: ERROR"
+  else do
+    -- Write token and class to token file
+  
+    -- Continue analysis
+    if ch == end_file then putStrLn "Lexicalli: Success" -- file ended and we're in a successful state
+    else do
+      -- Resume the parse in the state from the character
+      -- The reason for this conditional is that I don't want a leading whitespace
+      --  in my next token
+      if elem ch whitespace then do
+        tok <- scan (alphabet, state_transition, 0, is_end) [] get_char_cast skip_state
+        log_and_recurse tok
+      else do
+        tok <- scan (alphabet, state_transition, (state_transition 0 ch), is_end) [ch] get_char_cast skip_state
+        log_and_recurse tok
+  
+-- Called by C
 run_scanner :: IO ()
 run_scanner = do
-  print (state_matrix!!1!!1)
-  x <- get_char_cast
-  print x
-  a <- get_char
-  b <- get_char
-  c <- get_char
-  putChar (castCCharToChar a)
-  putChar (castCCharToChar b)
-  putChar (castCCharToChar c)
-  j <- newCString "Joe"
-  f <- newCString "5"
-  print (put_token j (CInt 0) f (CInt 1))
+  -- Get the first token
+  tok1 <- scan (alphabet, state_transition, 0, is_end) [] get_char_cast skip_state
   
+  -- Continue the token scan
+  log_and_recurse tok1
+  
+-- Tell the compiler to generate C headers called "stubs" to interface
+-- The linker will take those stubs and the object files from compiled Haskell code and package them together
+--  for the final app
 foreign export ccall run_scanner :: IO ()
