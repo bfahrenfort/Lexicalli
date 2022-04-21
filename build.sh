09d84d850e159905e2b62ee0ccb0da16b7970b42
OCDkirby <mangacreeperyt@gmail.com>
2022-04-21 09:58:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
+mkdir bin
 ghc -c -O src/haskell/** -outputdir tmp -stubdir include/stubs -Iinclude && \
 ghc --make src/c/* src/haskell/* -no-hs-main -outputdir tmp -Iinclude -o bin/calli
